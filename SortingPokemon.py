d0fb5d5704601ba2a40ffcff68d846fbbece58de
Matthew <mrwhalen22@gmail.com>
2022-02-10 12:28:31 -0500

    Fixed quick sort and added plot charts to display count data

--- SortingPokemon.py
+++ SortingPokemon.py
@@ -1,9 +1,12 @@
 # Pokemon Sorting Assignment 1
 # BY: Matthew Whalen
 
+from cProfile import label
 import sys
 import os
 from typing import Tuple
+from matplotlib import pyplot as plt
+
 
 def insertion_sort(data) -> int:
     count = 0
@@ -16,7 +19,6 @@ def insertion_sort(data) -> int:
             count += 1
         data[j+1] = key
     return count
-
 
 def merge_sort(data) -> int:
     count = 0
@@ -51,33 +53,20 @@ def merge_sort(data) -> int:
             data[k] = R[j]
             j += 1
             k += 1
-
     return count
-
 
 def partition(data, begin, end) -> Tuple[int, int]:
     count = 0
-    pivot_index = begin
-    pivot = data[pivot_index]
-    while(begin < end):
-        while(begin < len(data)):
-            if(data[begin][1] <= pivot[1]):
-                begin+=1
-                count+=1
-            elif(data[begin][1] == pivot[1] and data[begin][0]<= pivot[0]):
-                begin+=1
-                count+=1
-            else: break
-        while(data[end][1] > pivot[1]):
-            end-=1
+    j = begin-1
+    pivot = data[end]
+    
+    for i in range(begin, end):
+        if((data[i][1] < pivot[1]) or ((data[i][1] == pivot[1]) and  (data[i][0] < pivot[0]))):
             count+=1
-
-        if(begin < end):
-            data[begin], data[end] = data[end], data[begin]
-
-    data[end], data[pivot_index] = data[pivot_index], data[end]
-
-    return end, count
+            j+=1
+            data[i], data[j] = data[j], data[i]
+    data[j+1], data[end] = data[end], data[j+1]
+    return j+1, count
 
 def quick_sort(data, begin, end) -> int:
     count = 0
@@ -89,7 +78,6 @@ def quick_sort(data, begin, end) -> int:
         count += quick_sort(data, div+1, end)
     return count
 
-
 def isSorted(data) -> bool:
     for i in range(len(data)-1):
         if(data[i][1] > data[i+1][1]):
@@ -100,7 +88,6 @@ def isSorted(data) -> bool:
             return False
     return True
 
-
 def main() -> int:
     #CHECK FOR ARGUMENTS
     options = sys.argv
@@ -108,7 +95,8 @@ def main() -> int:
         if(options[1].upper() != "MERGE" and options[1].upper() != "INSERTION" and options[1].upper() != "QUICK"):
             print("Usage: " + sys.argv[0] + " <Sort Type>(Merge, Insertion, Quick)")
             return 0
-    
+
+    #INIT VARS AND PARSE CSV FILENAMES    
     pokemon_data = {}
     sort_counts = {}
     csv_files = list(filter(lambda x: ".csv" in x, os.listdir("./"))) 
@@ -125,52 +113,83 @@ def main() -> int:
         pokemon_data[filename] = data
         file.close()
                      
-
-
-
     #SORT ALL DATA LISTS
     if(len(options) == 1):
         # INSERTION SORT BY DEFAULT
         print("Sorting with Insertion Sort. Please give an option for other sorting methods.")
         for file in csv_files:
             sort_counts[file] = insertion_sort(pokemon_data[file])
+            if(isSorted(pokemon_data[file])): print(file + ": SUCCESS")
+            else: print(file + ": FAILURE")
     elif(options[1].upper() == "INSERTION"):
         # USE INSERTION SORT
         print("Sorting with Insertion Sort.")
         for file in csv_files:
             sort_counts[file] = insertion_sort(pokemon_data[file])
+            if(isSorted(pokemon_data[file])): print(file + ": SUCCESS")
+            else: print(file + ": FAILURE")
     elif(options[1].upper() == "MERGE"):
         # USE MERGE SORT
         print("Sorting using Merge Sort.")
         for file in csv_files:
             sort_counts[file] = merge_sort(pokemon_data[file])
+            if(isSorted(pokemon_data[file])): print(file + ": SUCCESS")
+            else: print(file  + ": FAILURE")
     elif(options[1].upper() == "QUICK"):
         # USE QUICK SORT
         print("Sorting using Quick Sort.")
         for file in csv_files:
             sort_counts[file] = quick_sort(pokemon_data[file], 0, len(pokemon_data[file])-1)
+            if(isSorted(pokemon_data[file])): print(file + ": SUCCESS")
+            else: print(file + ": FAILURE")
     else:
         print("ERROR")
-        return -1
+        return -1  
+
         
-    
 
-
-
-
-    #TEST SORTED DATA LISTS
+    #EXPORT COUNT DATA
+    output = open(options[1][0].upper() + "SortCounts.txt","w")
     for file in csv_files:
-        if(isSorted(pokemon_data[file])): print("SUCCESS")
-        else: print("FAILURE")
+        output.writelines(("INSERTION" if len(options) < 2 else options[1].upper()) + " SORT " + file + ": number of values = " + str(len(pokemon_data[file])) + ", comparison count = " + str(sort_counts[file]) + "\n")
+        #print(("INSERTION" if len(options) < 2 else options[1].upper()) + " SORT " + file + " comparison count: " + str(sort_counts[file]))
+    
+    print("Data exported to text file.")
 
-    output = open("SortCounts.txt","w")
+    pxRa=[]
+    pyRa=[]
+    pxRe=[]
+    pyRe=[]
+    pxS=[]
+    pyS=[]
+    i = 0
     for file in csv_files:
-        output.writelines(("INSERTION" if len(options) < 2 else options[1].upper()) + " SORT " + file + " comparison count: " + str(sort_counts[file]) + "\n")
-        print(("INSERTION" if len(options) < 2 else options[1].upper()) + " SORT " + file + " comparison count: " + str(sort_counts[file]))
-    
-    
+        if(i < 3):
+            pxRa.append(len(pokemon_data[file]))
+            pyRa.append(sort_counts[file])
+            
+        elif(i < 6):
+            pxRe.append(len(pokemon_data[file]))
+            pyRe.append(sort_counts[file])
+        else:
+            pxS.append(len(pokemon_data[file]))
+            pyS.append(sort_counts[file])
+        i+=1
+    plt.plot(pxRa,pyRa, label = "Random")
+    plt.plot(pxRe,pyRe, label = "Reverse")
+    plt.plot(pxS,pyS, label = "Sorted")
+
+    plt.legend()
+    plt.xlabel("Number of Values")
+    plt.ylabel("Number of Comparisons Made")
+    plt.title("Sort Comparisons - " + options[1].upper())
+
+    print("Displaying Graph.")
+    plt.show()
+
+
+
     return 1
-
 
 if __name__ == "__main__":
     sys.exit(main())
